f0889fd3d017603c40eba12090a974b4d51ceee0
Andy Sun <andysun9001@gmail.com>
2024-08-08 14:18:36 -0400

    initial commit

---
+++ src/test/java/com/example/stocktrackback/StocktrackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.stocktrackback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StocktrackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
