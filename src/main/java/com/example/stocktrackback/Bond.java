3107fcf55ee5785c01b61e0d901e7ff6a1e9d43c
Andy Sun <andysun9001@gmail.com>
2024-08-09 14:25:02 -0400

    refactor security classes

--- src/main/java/com/example/stocktrackback/Bond.java
+++ src/main/java/com/example/stocktrackback/Bond.java
@@ -3,14 +3,14 @@ package com.example.stocktrackback;
 public class Bond extends Security{
     private double interestRate;
 
-    public Bond(String id, String name, int quantity, int purchase_price, int current_price) {
-        super(id, name, quantity, purchase_price, current_price);
+    public Bond(String id, String name, double interestRate) {
+        super(id, name);
         this.interestRate = interestRate;
     }
 
     @Override
     public double getCurrentValue() {
-        return getCurrent_price() * getQuantity();
+        return 0; //TODO call yahoo api
     }
 
     @Override
